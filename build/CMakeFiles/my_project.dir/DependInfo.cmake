f8d266222ad402cd4e1bfc3baf6ca574429c295d
Thien <nguyenhthien2001@gmail.com>
2024-02-16 12:41:13 -0600

    add ignored

---
+++ build/CMakeFiles/my_project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/thien/Documents/IE_ROS/Project_3/src/Person.cpp" "CMakeFiles/my_project.dir/src/Person.cpp.o" "gcc" "CMakeFiles/my_project.dir/src/Person.cpp.o.d"
+  "/home/thien/Documents/IE_ROS/Project_3/src/main.cpp" "CMakeFiles/my_project.dir/src/main.cpp.o" "gcc" "CMakeFiles/my_project.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
